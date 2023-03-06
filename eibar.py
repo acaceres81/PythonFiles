edb85e78ec7827618a409d01a9c082e27218892b
asierferrero <ferrero.asier@uni.eus>
2023-03-06 12:43:34 +0100

    06/03/2023

--- eibar.py
+++ eibar.py
@@ -13,35 +13,40 @@ while a == 1:
     option = input("Enter your option (1-3): ")
 
     if option == "1":
-            fnames = ['id', 'day', 'time', 'location', 'opponent', 'result']
+        fnames = ['id', 'day', 'time', 'location', 'opponent', 'result']
 
-            while m == 1:
-                i = input("Write the match id: ")
-                d = input("Write the match day (dd.mm.yyyy): ")
-                t = input("Write the match time (00:00): ")
-                p = input("Write where the match was played (Enter C or F): ")
-                o = input("Write the opponent: ")
-                r = input("Write the match result (0:0): ")
+        f = 1
+        while f == 1:
+            i = input("Write the match id: ")
+            d = input("Write the match day (dd.mm.yyyy): ")
+            t = input("Write the match time (00:00): ")
+            p = input("Write where the match was played (Enter C or F): ")
+            o = input("Write the opponent: ")
+            r = input("Write the match result (0:0): ")
 
-                dict = {'id': i, 'day': d, 'time': t, 'location': p, 'opponent': o, 'result': r}
+            dict = {'id': i, 'day': d, 'time': t, 'location': p, 'opponent': o, 'result': r}
 
-                with open('eibar.csv', 'a+') as csv_file:
-                    dict_object = csv.DictWriter(csv_file, fieldnames=fnames)
+            with open('eibar.csv', 'a+') as csv_file:
+                dict_object = csv.DictWriter(csv_file, fieldnames=fnames)
 
-                    dict_object.writerow(dict)
+                dict_object.writerow(dict)
 
+            b = 1
+            while b == 1:
                 a = input("Do you want to enter another match? (y/n): ")
 
                 if a == 'n':
-                    a = 0
+                    f = 0
+                    a = 1
+                    b = 0
 
                 elif a == 'y':
-                    a = 1
+                    f = 1
+                    b = 0
 
                 else:
                     print("You have to enter 'y' or 'n'")
-                    a = 1
-            f.close()
+                    b = 1
 
     elif option == "2":
         with open('eibar.csv', "r") as csv_file:
